0cb2d9a4514c8afdc11fbd8302bffd86fa312c5b
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-14 18:07:20 -0500

    Write test for map facade

--- spec/facades/map_facade_spec.rb
+++ spec/facades/map_facade_spec.rb
@@ -3,5 +3,9 @@ require 'rails_helper'
 RSpec.describe MapFacade, :vcr do
   let(:location) { 'Sparta, NJ' }
 
-  it 'returns result from get_coordinates'
+  it 'returns result from get_coordinates' do
+    result = MapFacade.get_coordinates(location)
+
+    expect(result).to be_a Location
+  end
 end
