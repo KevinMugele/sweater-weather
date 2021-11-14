91d6f83d0bbf9e61d5b1138d5873df85d2698d43
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-14 18:07:09 -0500

    Write test for map client

--- spec/clients/map_client_spec.rb
+++ spec/clients/map_client_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe MapClient, :vcr do
-  it 'returns result from fetch request'
+  let(:location) { 'Sparta, NJ' }
+  let(:url) { '/geocoding/v1/address' }
+
+  it 'returns result from get coordinates' do
+    result = MapClient.get_coordinates(location, url)
+
+    expect(result).to be_a Hash
+    expect(result[:results].first[:providedLocation][:location]).to eq(location)
+    expect(result[:results].first[:locations].first[:displayLatLng]).to be_a Hash
+  end
 end
