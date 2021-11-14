9ccae70f80b3098485c1b08ad99baa52a00607b9
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-14 18:08:03 -0500

    Write test for weather client

--- spec/clients/weather_client_spec.rb
+++ spec/clients/weather_client_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe WeatherClient, :vcr do
-  it 'returns result from fetch request'
+  let(:latlong) { {
+    longitude: '-74.636612',
+    latitude: '41.03608'
+    } }
+
+  it 'returns result from get forecast' do
+    result = WeatherClient.get_forecast(latlong)
+
+    expect(result).to be_a Hash
+    expect(result).to have_key(:current)
+    expect(result).to have_key(:hourly)
+    expect(result).to have_key(:daily)
+  end
 end
