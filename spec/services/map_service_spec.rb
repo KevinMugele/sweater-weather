db2b62a5ddd7cebceef0f64b2d8ac8e15982e671
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-14 18:09:11 -0500

    Write test for map service

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -3,5 +3,11 @@ require 'rails_helper'
 RSpec.describe MapService, :vcr do
   let(:location) { 'Sparta, NJ' }
 
-  it 'returns result from get_coordinates'
+  it 'returns result from get_coordinates' do
+    result = MapService.get_coordinates(location)
+
+    expect(result).to be_a Hash
+    expect(result[:results].first[:locations].first[:displayLatLng][:lat]).to eq(41.03608)
+    expect(result[:results].first[:locations].first[:displayLatLng][:lng]).to eq(-74.636612)
+  end
 end
