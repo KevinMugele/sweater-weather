9f2fb2925fc32806ebbae59121f73d73b21e541d
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-15 12:41:16 -0500

    Update request spec file and corresponding vcr file

--- spec/requests/api/v1/activities_spec.rb
+++ spec/requests/api/v1/activities_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'API::V1::Activities API' do
   describe 'GET /api/v1/forecast', :vcr do
-    let(:location) { 'Sparta, nj' }
+    let(:destination) { 'Sparta, nj' }
     let(:result) { JSON.parse(response.body, symbolize_names: true) }
 
-    it 'returns the forecase for the given location' do
-      get "/api/v1/activities?destination=#{location}"
+    it 'returns the forecase for the given destination' do
+      get "/api/v1/activities?destination=#{destination}"
 
       expect(response).to be_successful
 
