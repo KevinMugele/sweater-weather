b2cec70838645905e7a18b8ab4059a6507248ccf
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-15 10:37:13 -0500

    Update user model to handle generating api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+  has_many :api_keys
+  has_secure_password
+
+  validates :email, uniqueness: true, presence: true
+  validates :password_confirmation, presence: true
+
+  after_commit :generate_api_key, on: :create
+
+  def api_key
+    api_keys.api_token
+  end
+
+  private
+
+  def generate_api_key
+    ApiKey.create!(user: self, token: SecureRandom.hex)
+  end
 end
