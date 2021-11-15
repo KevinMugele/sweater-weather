21a23b0753815deec54e58ffb7b02a7e9dd88d44
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-15 12:38:42 -0500

    Add additional testing for request spec

--- spec/requests/api/v1/activities_spec.rb
+++ spec/requests/api/v1/activities_spec.rb
@@ -9,6 +9,16 @@ RSpec.describe 'API::V1::Activities API' do
       get "/api/v1/activities?destination=#{location}"
 
       expect(response).to be_successful
+
+      expect(result).to be_a Hash
+      expect(result[:data]).to be_a Hash
+      expect(result[:data][:type]).to eq('activities')
+
+      attributes = result[:data][:attributes]
+
+      expect(attributes).to have_key(:activities)
+      expect(attributes).to have_key(:destination)
+      expect(attributes).to have_key(:forecast)
     end
   end
 end
