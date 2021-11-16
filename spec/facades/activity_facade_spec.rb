2e1eb63c59510f61ae36afb9e13a754dea8430c7
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-16 12:29:40 -0500

    Update activity facade tests

--- spec/facades/activity_facade_spec.rb
+++ spec/facades/activity_facade_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe ActivityFacade, :vcr do
-  let(:destination) { 'Sparta, NJ' }
+  it 'return results from get_activity' do
+    result = ActivityFacade.get_activity('Sparta, NJ')
+
+    expect(result).to be_a Activities
+    expect(result.activities.size).to eq 2
+  end
 
   it 'return results from get_activity' do
-    result = ActivityFacade.get_activity(destination)
+    result = ActivityFacade.get_activity('Miami, FL')
+
+    expect(result).to be_a Activities
+    expect(result.activities.size).to eq 2
+  end
+
+  it 'return results from get_activity' do
+    result = ActivityFacade.get_activity('San Francisco, CA')
 
     expect(result).to be_a Activities
     expect(result.activities.size).to eq 2
