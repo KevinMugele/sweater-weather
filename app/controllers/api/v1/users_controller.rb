8003ec11628b9878939b70ba83a65a755ad6c788
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-16 12:29:29 -0500

    Add a 201 status code

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.create!(user_params)
 
-    render json: UserSerializer.new(user), status: :created
+    render json: UserSerializer.new(user), status: 201
   end
 
   private
