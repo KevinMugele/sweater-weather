316510972bc48d2c3643b92d75c24ca4516d0a44
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-15 19:04:15 -0500

    Add a get directions method to map service

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -4,5 +4,10 @@ class MapService
       url = '/geocoding/v1/address'
       MapClient.get_coordinates(location, url)
     end
+
+    def get_directions(data_hash)
+      url = '/directions/v2/route'
+      MapClient.get_directions(url, data_hash)
+    end
   end
 end
