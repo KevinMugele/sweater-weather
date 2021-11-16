a3a9706282115653bec045e36bc8069e91c729d3
Kevin Mugele <kevin.mugele@gmail.com>
2021-11-15 19:05:23 -0500

    Create a road trip facade

---
+++ app/facades/road_trip_facade.rb
@@ -0,0 +1,13 @@
+class RoadTripFacade
+  class << self
+    def get_road_trip(params)
+      directions = MapFacade.get_directions(params)
+      return RoadTrip.new(params) if directions.nil?
+
+      forecast = WeatherFacade.get_forecast(latitude: directions.end_latitude,
+                                        longitude: directions.end_longitude)
+
+      RoadTrip.new(directions, forecast)
+    end
+  end
+end
